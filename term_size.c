1d08760309d5c3ca49f05ccba7e9f94d1d41f7ef
Remi CARRASSUS <recarras@e1r7p5.42.fr>
2017-07-07 01:34:49 +0200

    term size

---
+++ term_size.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   touch.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: recarras <rcarrass42@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/07 00:21:44 by recarras          #+#    #+#             */
+/*   Updated: 2017/07/07 00:44:55 by recarras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>    // strerror
+#include <errno.h>     // errno
+#include <fcntl.h>     // open(), O_EVTONLY, O_NONBLOCK
+#include <unistd.h>    // close()
+#include <sys/ioctl.h> // ioctl()
+
+int main() {
+	int tty_fd = open("/dev/tty", O_EVTONLY | O_NONBLOCK);
+	if (tty_fd == -1) {
+		fprintf(stderr, "Opening `/dev/tty` failed (%d): %s\n", errno, strerror(errno));
+		return 1;
+	}
+
+	struct winsize ws;
+	int result = ioctl(tty_fd, TIOCGWINSZ, &ws);
+	close(tty_fd);
+
+	if (result == -1) {
+		fprintf(stderr, "Getting the size failed (%d): %s\n", errno, strerror(errno));
+		return 1;
+	}
+
+	fprintf(stdout, "%d\n%d\n", ws.ws_col, ws.ws_row);
+	return 0;
+}
